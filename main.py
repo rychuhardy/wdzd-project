0cad69fc47aaf2df9a0c4f00c3c94893bedb7c2f
Ryszard Sikora <ryszard.sikora7@gmail.com>
2018-05-22 11:11:24 +0200

    data for background deploy

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import networkx as nx
 import numpy as np
 import json
+import time
+import sendgrid
+import os
 
 graph_path = './data/dataset3/deezer_clean_data/HR_edges.csv'
-#graph_path = './data/testdata.txt'
+graph_path = './data/testdata.txt'
+log_file = open("log.txt", 'w', 10)
+start_time = time.perf_counter()
+graph = nx.read_edgelist(graph_path, delimiter=' ')
 
-graph = nx.read_edgelist(graph_path, delimiter=',')
-
-print("Loaded graph with {0} nodes and {1} edges".format(graph.number_of_nodes(), graph.number_of_edges()))
+log_file.write("Loaded graph with {0} nodes and {1} edges\n".format(graph.number_of_nodes(), graph.number_of_edges()))
 
 graph_diameter = nx.diameter(graph)
+
+after_diameter_time = time.perf_counter()
+log_file.write("Finished calculating diameter. Time elapsed: {}s\n".format(after_diameter_time - start_time))
+
 values = [0]*graph_diameter
 result = {key: list(values) for key in graph.nodes()}
 
@@ -17,7 +25,7 @@ result = {key: list(values) for key in graph.nodes()}
 idx = 0
 for node in graph:
     # Logging
-    print("Node {0} out of {1}".format(idx+1, graph.number_of_nodes()))
+    log_file.write("Node {0} out of {1}. Time elapsed {2}s\n".format(idx+1, graph.number_of_nodes(), time.perf_counter() - start_time))
     idx = idx + 1
 
     # Calculate k-distances from node
@@ -38,10 +46,46 @@ for node in graph:
             # dist - 1 because indexing starts at 0, so it's easier. It output this means that neighbours have distance equal to zero.
             result[node][dist - 1] = result[node][dist - 1] + 1
 
-print(result)
+#print(result)
+
+log_file.write("Finished algorithm. Time elapsed {}s. Starting writitng result to json.\n".format(time.perf_counter() - start_time))
 
 # Save result
 json_result = json.dumps(result)
 
 with open("result.json", "w") as output_file:
     output_file.write(json_result)
+
+log_file.write("Done.\n")
+
+
+os.environ['SENDGRID_API_KEY'] = "apikey"
+
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+data = {
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "receiver"
+        }
+      ],
+      "subject": "Finished calculating fingerprint"
+    }
+  ],
+  "from": {
+    "email": "python@azure.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "It's done"
+    }
+  ]
+}
+response = sg.client.mail.send.post(request_body=data)
+
+# log_file.write(response.body)
+# log_file.write(response.headers)
+
+log_file.close()
